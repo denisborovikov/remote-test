6e2a84fe6bc37a21191ca365311fbd227070070f
Denis Borovikov <metaller.ru@gmail.com>
2021-02-23 15:35:56 -0700

    Add API URL variable

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = process.env.API || 'http://localhost:4000';
 axios.defaults.baseURL = BASE_URL;
 
 async function get(url, params) {
